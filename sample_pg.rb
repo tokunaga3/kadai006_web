831d01c03e40f32f1b9638bda9e07de817e3ac8d
mutsukitokunaga <mutsukitokunaga@outlook.jp>
2020-11-26 02:18:51 +0000

    課題提出2

--- sample_pg.rb
+++ sample_pg.rb
@@ -8,12 +8,17 @@ begin
   # connection変数を使いPostgreSQLを操作する
   # .execで、goyaDBに"select weight, give_for from crops;"
   # のSQLの命令文を直接実行し、その結果をresult変数に格納する
-  result = connection.exec("SELECT * FROM crops WHERE give_for != '自家消費'  AND quality = false;")
+  result = connection.exec(" SELECT * FROM crops WHERE give_for != '自家消費';")
+  quality = connection.exec(" SELECT * FROM crops WHERE quality = false;")
   # 取り出した各行を処理する
   result.each do |record|
       # 各行を取り出し、putsでターミナル上に出力する
       puts "ゴーヤの大きさ：#{record["weight"]}　売った相手：#{record["give_for"]}"
   end
+  # quality.each do |reco|
+  #     # 各行を取り出し、putsでターミナル上に出力する
+  #     puts "ゴーヤの大きさ：#{reco["weight"]}　売った相手：#{reco["give_for"]}"
+  # end
 ensure
   # 最後に.finishでデータベースへのコネクションを切断する
   connection.finish
